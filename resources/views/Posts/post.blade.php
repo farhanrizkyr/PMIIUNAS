b9ef9c107ccfd7c6fdfb33d928ae781cb9719e50
Farhan Rizky Ramadhan <farhanrizkyramadhan99@gmail.com>
2023-11-03 08:53:30 +0700

    Update

--- resources/views/Posts/post.blade.php
+++ resources/views/Posts/post.blade.php
@@ -92,7 +92,7 @@
 <div class="modal fade" id="detail-artikel{{$data->id}}" tabindex="-1" role="dialog" aria-labelledby="exampleModalLongTitle" aria-hidden="true">
   <div class="modal-dialog" role="document">
     <div class="modal-content">
-      <div class="modal-header bg-primary">
+      <div class="modal-header text-white bg-primary">
         <h5 class="modal-title" id="exampleModalLongTitle"><i class="fas fa-newspaper"></i> Postingan Draft Artikel</h5>
         <button type="button" class="close" data-dismiss="modal" aria-label="Close">
           <span aria-hidden="true">&times;</span>
