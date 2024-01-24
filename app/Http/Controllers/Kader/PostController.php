6d0276bedc3294fd1d8847eebe742e715cfa76be
Farhan Rizky Ramadhan <67540962+farhanrizkyr@users.noreply.github.com>
2024-01-24 13:40:01 +0700

    Update PostController.php

--- app/Http/Controllers/Kader/PostController.php
+++ app/Http/Controllers/Kader/PostController.php
@@ -24,7 +24,7 @@ class PostController extends Controller
 }
     public function index()
     {
-        $categories=Category::with('category')->orderby('created_at','desc')->get();
+        $categories=Category::with('category')->latest()->get();
         $kader_id=Auth::user()->id;
       $posts=Post::orderby('created_at','desc')->where('kader_id',auth()->user()->id)->get();
       
