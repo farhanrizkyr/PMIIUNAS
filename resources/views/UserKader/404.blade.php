05568ec76215650e0853ae2c976496504475e183
Farhan Rizky Ramadhan <farhanrizkyramadhan99@gmail.com>
2024-02-23 11:04:13 +0700

    update

--- resources/views/UserKader/404.blade.php
+++ resources/views/UserKader/404.blade.php
@@ -4,6 +4,6 @@
 @section('content')
 <div class="card" style="padding:35px;">
 	<h1 class="text-center">404</h1>
-	<h5 class="text-center">Content Yang Anda Cari Tidak Ada!!!</h5>
+	<h5 class="text-center">Content Yang Anda Cari Tidak Ada!!</h5>
 </div>
 @endsection
