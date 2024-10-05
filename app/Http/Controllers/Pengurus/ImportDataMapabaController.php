14eb37b45d25a51545a817a7f7f24015d8ebc0da
Farhan Rizky  Ramadhan <farhanrizkyramadhan99@gmail.com>
2024-10-05 23:09:15 +0700

    bugfix(controller Import):memperbaiki controller double function import controller

--- app/Http/Controllers/Pengurus/ImportDataMapabaController.php
+++ app/Http/Controllers/Pengurus/ImportDataMapabaController.php
@@ -6,7 +6,6 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\Mapaba;
 use App\Models\Tahun;
-use App\Models\Tahun;
 use App\Models\Progdi;
 class ImportDataMapabaController extends Controller
 {
@@ -50,7 +49,7 @@ class ImportDataMapabaController extends Controller
      * @param  \App\Models\ImportDataMapaba  $importDataMapaba
      * @return \Illuminate\Http\Response
      */
-    public function show()
+   
     public function show()
     {
         //
@@ -63,7 +62,7 @@ class ImportDataMapabaController extends Controller
      * @return \Illuminate\Http\Response
      */
     public function edit()
-    public function edit()
+
     {
         //
     }
@@ -87,7 +86,7 @@ class ImportDataMapabaController extends Controller
      * @param  \App\Models\ImportDataMapaba  $importDataMapaba
      * @return \Illuminate\Http\Response
      */
-    public function  list_tahun_mahasiswa(Tahun $id)
+  
     public function  list_tahun_mahasiswa(Tahun $id)
     {
        $datas=$id->tahuns->where('archive','approve');
