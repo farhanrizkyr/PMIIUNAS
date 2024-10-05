7d35f05a1de40155337a68290d0ec434f8c7c2ca
Farhan Rizky  Ramadhan <farhanrizkyramadhan99@gmail.com>
2024-10-05 14:14:32 +0700

    feat(filter tahun angkatan):membuat fitur filter tahun angkatan mapaba

--- resources/views/ImportDataMapaba/index.blade.php
+++ resources/views/ImportDataMapaba/index.blade.php
@@ -35,10 +35,10 @@
             <th>{{$no++;}}</th>
             <td>{{$list->name}}</td>
             <td>{{$list->kampus}}</td>
-            <td>{{optional($list->tahun)->tahun??null}}</td>
+            <td>{{optional($list->tahun)->tahun??null}} <a href="/history-datamapaba/list-tahun-angkatan/{{$list->tahun->id ?? ''}}"> (Lihat) </a></td>
             <td>{{optional($list->progdi)->progdi??null}}<a href="/history-datamapaba/list-mahasiswa-program-studi/{{$list->progdi->id ?? ''}}"> (Lihat)</a></td>
             <td>{{$list->hp}}</td>
-            <td><a href="history-datamapaba/edit-arsip/{{$list->id}}" class="btn btn-info btn-sm"><i class="fas fa-edit"></i> Edit</a>
+            <td><a href="/history-datamapaba/edit-arsip/{{$list->id}}" class="btn btn-info btn-sm"><i class="fas fa-edit"></i> Edit</a>
 
            <form method="post" action="/history-datamapaba/delete-history/{{$list->id}}" class="d-inline">
              @csrf
---
+++ resources/views/ImportDataMapaba/list_tahun.blade.php
@@ -0,0 +1,57 @@
+@extends('master.tampil')
+@section('tittle')
+List Data Program Studi: {{$id->tahun}}
+@endsection
+@section('page','Data Arsip MAPABA')
+@section('contentt')
+List Data Program Tahun Angkatan Mahasiswa: {{$id->tahun}}
+@endsection
+@section('content')
+<a href="/history-datamapaba/" class="btn btn-success mb-4"><i class="fas fa-arrow-left"></i> Kembali Ke List Data History Mapaba</a>
+<div class="table-wrapper">
+
+  <table id="tabel-data" class="table table-striped table-hover" width="100%" cellspacing="0">
+        <thead>
+            <tr>
+                <th>No</th>
+                <th>Nama Mahasiswa</th>      
+            </tr>
+        </thead>
+        
+        <tbody>
+          @php $no=1;@endphp
+          @foreach($datas as $list)
+           <tr>
+           	<th>{{$loop->iteration}}</th>
+           	<td>{{$list->name}}</td>
+           	@endforeach
+           </tr>
+        
+        </tbody>
+
+
+
+    </table>
+  </div>
+  
+
+  
+
+
+
+
+     <style>
+ .table-wrapper {
+
+  max-width: 100%;
+  overflow-x: auto;
+  -webkit-overflow-scrolling: touch;
+}
+	 </style>
+	 
+	 	 <script>
+  function refresh(){
+        window.location.reload("Refresh")
+      }
+</script>
+@endsection
